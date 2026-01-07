9eae0203939a50f6c28cc43450d519f20a6a7c60
luv <muyogalega@gmail.com>
2026-01-07 07:42:40 -0600

    update variable.tf

--- variables.tf
+++ variables.tf
@@ -6,8 +6,8 @@ variable "aws_region" {
 
 variable "target_name" {
   type        = string
-  description = "Name prefix for resources created and destroyed by Terraform"
-  default     = "jenkins-target"
+  description = "Name prefix for target resources created/destroyed by Terraform"
+  default     = "jenkins-al2023-target"
 }
 
 variable "instance_type" {
@@ -18,4 +18,33 @@ variable "instance_type" {
 
 variable "key_name" {
   type        = string
-  description = "Existing EC2 Key Pa
+  description = "Existing EC2 Key Pair name for SSH access"
+  default     = "ec2-jenkins-cicd"
+}
+
+variable "ssh_cidr" {
+  type        = string
+  description = "CIDR allowed to SSH (port 22) to the target instance. Recommended: your_public_ip/32"
+  default     = "0.0.0.0/0"
+}
+
+variable "app_cidr" {
+  type        = string
+  description = "CIDR allowed to access the target app on port 8080. Recommended: your_public_ip/32"
+  default     = "0.0.0.0/0"
+}
+
+variable "availability_zone_suffix" {
+  type        = string
+  description = "Availability Zone suffix, e.g. 'a' makes us-east-2a"
+  default     = "a"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Common tags applied to resources"
+  default = {
+    ManagedBy = "Terraform"
+    Project   = "JenkinsPractice"
+  }
+}
